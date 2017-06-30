13abcaf45aa9822b8717055463eb2e40f0af0412
Miloud Haimoune <mhaimoune@wiley.com>
2017-06-30 12:07:07 +0100

    getting list of published issues from the service

--- src/main/java/com/ojs/service/content/v1/service/DBIssueService.java
+++ src/main/java/com/ojs/service/content/v1/service/DBIssueService.java
@@ -1,16 +1,35 @@
 package com.ojs.service.content.v1.service;
 
+import com.ojs.service.content.v1.domain.IssueRepository;
+import com.ojs.service.content.v1.domain.Issues;
 import com.ojs.service.content.v1.dto.Issue;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
 
-
+@Service
 public class DBIssueService implements IssueService {
 
+    @Autowired
+    private final IssueRepository issueRepository;
+
+    public DBIssueService(IssueRepository issueRepository) {
+        this.issueRepository = issueRepository;
+    }
 
     @Override
     public List<Issue> getPublishedIssues() {
-        return new ArrayList<>();
+
+        ArrayList<Issue> issues = new ArrayList<>();
+
+        List<Issues> domainIssues = issueRepository.findByPublished(true);
+
+        for (Issues di : domainIssues) {
+            issues.add(new Issue(0, 0));
+        }
+
+        return issues;
     }
 }
--- src/test/java/com/ojs/service/content/v1/service/DBIssueServiceTest.java
+++ src/test/java/com/ojs/service/content/v1/service/DBIssueServiceTest.java
@@ -1,33 +1,68 @@
 package com.ojs.service.content.v1.service;
 
 
+import com.ojs.service.content.v1.domain.IssueRepository;
+import com.ojs.service.content.v1.domain.Issues;
 import com.ojs.service.content.v1.dto.Issue;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
 
+import java.util.Arrays;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
+@RunWith(MockitoJUnitRunner.class)
 public class DBIssueServiceTest {
-
 
     IssueService issueService;
 
+    @Mock
+    private IssueRepository issueRepository;
+
     @Before
     public void setup() {
-        issueService = new DBIssueService();
+        issueService = new DBIssueService(issueRepository);
     }
 
-
     @Test
-    public void shouldReturnEmptyListWhenNoPublishedIssues() throws Exception {
-
+    public void getPublishedIssues_shouldReturnEmptyListWhenNoPublishedIssues() throws Exception {
 
         List<Issue> issues = issueService.getPublishedIssues();
 
         assertThat(issues.size()).isEqualTo(0);
 
     }
+
+    @Test
+    public void getPublishedIssues_shouldCallIssueRepositoryWithPublishedAsTrue() throws Exception {
+
+        List<Issue> issues = issueService.getPublishedIssues();
+
+        assertThat(issues.size()).isEqualTo(0);
+
+        verify(issueRepository).findByPublished(true);
+    }
+
+    @Test
+    public void getPublishedIssues_shouldReturnResultsWhenIssuesPublishedInDB() throws Exception {
+
+        when(issueRepository.findByPublished(true)).thenReturn(listOfDomainIssues());
+        List<Issue> issues = issueService.getPublishedIssues();
+
+        assertThat(issues.size()).isEqualTo(2);
+
+    }
+
+    private List<Issues> listOfDomainIssues() {
+
+        return Arrays.asList(new Issues(), new Issues());
+    }
+
 
 }
