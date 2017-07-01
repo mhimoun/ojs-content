113209254ca0049d24391c2fa7d4797ea576bb25
Miloud Haimoune <mhaimoune@wiley.com>
2017-07-01 12:08:28 +0100

    adding issue status and style file

--- src/main/java/com/ojs/service/content/v1/dto/Issue.java
+++ src/main/java/com/ojs/service/content/v1/dto/Issue.java
@@ -2,6 +2,9 @@ package com.ojs.service.content.v1.dto;
 
 import org.springframework.hateoas.ResourceSupport;
 
+import javax.persistence.Column;
+import java.util.Date;
+
 
 public class Issue extends ResourceSupport {
     private long journalId;
@@ -10,6 +13,18 @@ public class Issue extends ResourceSupport {
     private String number;
     private short year;
     private boolean published;
+    private boolean current;
+    private Date datePublished;
+    private Date dateNotified;
+    private Date lastModified;
+    private short accessStatus;
+    private Date openAccessDate;
+    private boolean showVolume;
+    private boolean showNumber;
+    private boolean showYear;
+    private boolean showTitle;
+    private String styleFileName;
+    private String originalStyleFileName;
 
     public Issue(long issueId, long journalId) {
         this.issueId = issueId;
@@ -43,6 +58,54 @@ public class Issue extends ResourceSupport {
         return published;
     }
 
+    public boolean isCurrent() {
+        return current;
+    }
+
+    public boolean isShowVolume() {
+        return showVolume;
+    }
+
+    public boolean isShowNumber() {
+        return showNumber;
+    }
+
+    public boolean isShowYear() {
+        return showYear;
+    }
+
+    public boolean isShowTitle() {
+        return showTitle;
+    }
+
+    public Date getDatePublished() {
+        return datePublished;
+    }
+
+    public Date getDateNotified() {
+        return dateNotified;
+    }
+
+    public Date getLastModified() {
+        return lastModified;
+    }
+
+    public short getAccessStatus() {
+        return accessStatus;
+    }
+
+    public Date getOpenAccessDate() {
+        return openAccessDate;
+    }
+
+    public String getStyleFileName() {
+        return styleFileName;
+    }
+
+    public String getOriginalStyleFileName() {
+        return originalStyleFileName;
+    }
+
     public void setJournalId(long journalId) {
         this.journalId = journalId;
     }
@@ -65,5 +128,53 @@ public class Issue extends ResourceSupport {
 
     public void setPublished(boolean published) {
         this.published = published;
+    }
+
+    public void setCurrent(boolean current) {
+        this.current = current;
+    }
+
+    public void setDatePublished(Date datePublished) {
+        this.datePublished = datePublished;
+    }
+
+    public void setDateNotified(Date dateNotified) {
+        this.dateNotified = dateNotified;
+    }
+
+    public void setLastModified(Date lastModified) {
+        this.lastModified = lastModified;
+    }
+
+    public void setAccessStatus(short accessStatus) {
+        this.accessStatus = accessStatus;
+    }
+
+    public void setOpenAccessDate(Date openAccessDate) {
+        this.openAccessDate = openAccessDate;
+    }
+
+    public void setShowVolume(boolean showVolume) {
+        this.showVolume = showVolume;
+    }
+
+    public void setShowNumber(boolean showNumber) {
+        this.showNumber = showNumber;
+    }
+
+    public void setShowYear(boolean showYear) {
+        this.showYear = showYear;
+    }
+
+    public void setShowTitle(boolean showTitle) {
+        this.showTitle = showTitle;
+    }
+
+    public void setStyleFileName(String styleFileName) {
+        this.styleFileName = styleFileName;
+    }
+
+    public void setOriginalStyleFileName(String originalStyleFileName) {
+        this.originalStyleFileName = originalStyleFileName;
     }
 }
--- src/test/java/com/ojs/service/content/v1/service/PopulateIssueFromDomainTest.java
+++ src/test/java/com/ojs/service/content/v1/service/PopulateIssueFromDomainTest.java
@@ -5,10 +5,12 @@ import com.ojs.service.content.v1.domain.Issues;
 import com.ojs.service.content.v1.dto.Issue;
 import org.junit.Test;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class PopulateIssueFromDomainTest {
-
 
 
     @Test
@@ -26,17 +28,103 @@ public class PopulateIssueFromDomainTest {
     public void shouldCreateIssueWithVolumeYearAndNumber() throws Exception {
 
         Issues issues = new Issues(10, 5);
-        issues.setVolume((short)45);
+        issues.setVolume((short) 45);
         issues.setYear((short) 1945);
         issues.setNumber("11");
         Issue issue = PopulateIssueFromDomain.valueOf(issues);
 
-        assertThat(issue.getVolume()).isEqualTo((short)45);
-        assertThat(issue.getYear()).isEqualTo((short)1945);
+        assertThat(issue.getVolume()).isEqualTo((short) 45);
+        assertThat(issue.getYear()).isEqualTo((short) 1945);
         assertThat(issue.getNumber()).isEqualTo("11");
+
+    }
+/*
+    private Date datePublished;
+
+    @Column
+    private Date dateNotified;
+
+    @Column
+    private Date lastModified;
+
+    @Column
+    private short accessStatus;
+
+    @Column
+    private Date openAccessDate;
+
+    @Column
+    private String styleFileName;
+
+    @Column
+    private String originalStyleFileName;*/
+
+    @Test
+    public void shouldCreateIssueWithAllBooleanFieldsFromDomain() throws Exception {
+
+        Issues issues = new Issues();
+        issues.setPublished(true);
+        issues.setCurrent(true);
+        issues.setShowNumber(true);
+        issues.setShowTitle(true);
+        issues.setShowVolume(true);
+        issues.setShowYear(true);
+        Issue issue = PopulateIssueFromDomain.valueOf(issues);
+
+        assertThat(issue.isPublished()).isEqualTo(true);
+        assertThat(issue.isCurrent()).isEqualTo(true);
+        assertThat(issue.isShowNumber()).isEqualTo(true);
+        assertThat(issue.isShowTitle()).isEqualTo(true);
+        assertThat(issue.isShowVolume()).isEqualTo(true);
+        assertThat(issue.isShowYear()).isEqualTo(true);
 
     }
 
 
+    /*
+
+    @Column
+    private short accessStatus;
+
+    @Column
+    private String styleFileName;
+
+    @Column
+    private String originalStyleFileName;*/
+
+    @Test
+    public void shouldCreateIssueWithAllDateFieldsFromDomain() throws Exception {
+        Date expectedDate = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss").parse("2017-06-29 15:18:00");
+
+        Issues issuesDomain = new Issues();
+        issuesDomain.setDatePublished(expectedDate);
+        issuesDomain.setDateNotified(expectedDate);
+        issuesDomain.setOpenAccessDate(expectedDate);
+        issuesDomain.setLastModified(expectedDate);
+
+        Issue issue = PopulateIssueFromDomain.valueOf(issuesDomain);
+
+        assertThat(issue.getDatePublished()).hasSameTimeAs(expectedDate);
+        assertThat(issue.getDateNotified()).hasSameTimeAs(expectedDate);
+        assertThat(issue.getOpenAccessDate()).hasSameTimeAs(expectedDate);
+        assertThat(issue.getLastModified()).hasSameTimeAs(expectedDate);
+
+    }
+
+    @Test
+    public void shouldCreateIssueWithSatusAndStyleFileFromDomain() throws Exception {
+
+        Issues issuesDomain = new Issues();
+        issuesDomain.setAccessStatus((short) 5);
+        issuesDomain.setStyleFileName("fileName");
+        issuesDomain.setOriginalStyleFileName("org fileName");
+
+        Issue issue = PopulateIssueFromDomain.valueOf(issuesDomain);
+
+        assertThat(issue.getAccessStatus()).isEqualTo((short) 5);
+        assertThat(issue.getStyleFileName()).isEqualTo("fileName");
+        assertThat(issue.getOriginalStyleFileName()).isEqualTo("org fileName");
+
+    }
 
 }
